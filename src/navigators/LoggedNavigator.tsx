837f8593601c717c864565a52613faf368cc4865
Agustin Gomez <agomez@midasconsultores.com.ar>
2023-08-28 23:11:47 -0300

    screens added to the navigator

--- src/navigators/LoggedNavigator.tsx
+++ src/navigators/LoggedNavigator.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from '../screens/Home';
+import AddTransaction from '../screens/AddTransaction';
 import SearchClientsScreen from '../screens/SearchClients';
 
 export type LoggedNavigatorParams = {
   HomeScreen: undefined;
+  AddTransaction: {
+    clientSelected?: string;
+  };
   SearchClientsScreen: {
     onClientItemPress?: () => void;
   };
@@ -20,6 +24,7 @@ const LoggedNavigator = () => (
     }}
   >
     <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    <Stack.Screen name="AddTransaction" component={AddTransaction} />
     <Stack.Screen name="SearchClientsScreen" component={SearchClientsScreen} />
   </Stack.Navigator>
 );
