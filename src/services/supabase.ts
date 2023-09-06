3e2b5989b8e7077905a1bd06d051c3b3f4751cfa
Agustin Gomez <agomez@midasconsultores.com.ar>
2023-09-05 23:51:22 -0300

    supabase warning fix

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,12 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://fswbrymefcjnbrzjifyc.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+  },
+});
 
 export default supabase;
